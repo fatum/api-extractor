5c0171bfbb887b984492a2d915965b2309562481
m.filippovich <m.filippovich@fun-box.ru>
2012-07-21 14:07:15 +0300

    add cookbooks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,4 @@
 # -*- encoding : utf-8 -*-
-Capistrano::Configuration::Namespaces::Namespace.class_eval do
-  def capture(*args)
-    parent.capture *args
-  end
-end
 
 set :application, "extractor"
 set :stages, ['production', 'staging']
@@ -30,13 +25,32 @@ set :roundsman_user, user
 set :ruby_version, '1.9.3-p125'
 
 role :app, "#{user}@localhost:2222", primary: true
+role :db, "#{user}@localhost:2222", primary: true
 
 before "deploy:setup", "roundsman:install"
 before "deploy:setup", "roundsman:chef:install"
 before "deploy:setup", "deploy:bundler_install"
 
 namespace :deploy do
+  task :server_setup, [:fetch_cookboks, :nginx_install, :mysql_install] do
+  end
+
+  task :fetch_cookboks do
+    run "git submodule update"
+  end
+
+  task :nginx_install, role: :app do
+    roundsman.run_list "recipe[runit]",
+                       "recipe[nginx]"
+  end
+
+  task :mysql_install, role: :db do
+    roundsman.run_list "recipe[mysql::server]"
+  end
+
   task :bundler_install, role: :app do
-    sudo "gem install bundle"
+    %w(bundle).each do |gem|
+      sudo "gem install #{gem} --quiet --no-ri --no-rdoc"
+    end
   end
 end
