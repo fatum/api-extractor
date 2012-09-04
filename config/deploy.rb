2526610dc226bccd002c80dae448c16d23c5ebfd
m.filippovich <m.filippovich@fun-box.ru>
2012-09-04 15:37:58 +0300

    setup deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,6 @@
 # -*- encoding : utf-8 -*-
 
-require 'deploy-recipes/deploy'
-
 set :application, "api-extractor"
 set :repository, "git@github.com:fatum/api-extractor.git"
 
-# Roundsman
-set :roundsman_user, user
+require 'deploy-recipes/deploy'
